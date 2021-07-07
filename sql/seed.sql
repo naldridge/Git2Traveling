e30c40f3f2baf3f08821943215c4c8ccbf331e7c
Daniel Agbenu <borgadee@gmail.com>
2021-07-07 16:40:54 -0400

    altered itinerary table

--- sql/seed.sql
+++ sql/seed.sql
@@ -4,13 +4,13 @@ VALUES
     ('Mike','Dean','go@travel.com', 'password');
 
 INSERT INTO trips 
-    (location, trip_name, trip_date)
+    (location, trip_name, trip_start_date, trip_end_date)
 VALUES
-    ('Houston','Brew trip','2021-07-06'),
-    ('Atlanta ','Summer trip', '2021-07-07'),
-    ('New York ','New year trip','2021-07-08'),
-    ('Dallas ','Summer','2021-07-09'),
-    ('Tampa ','Spring Break','2021-07-10');
+    ('Houston','Brew trip','2021-07-06', '2021-07-06'),
+    ('Atlanta ','Summer trip', '2021-07-07','2021-07-06'),
+    ('New York ','New year trip','2022-12-29', '2021-07-06'),
+    ('Dallas ','School reunion trip','2021-07-09','2021-07-06'),
+    ('Tampa ','Spring Break','2021-07-10', '2021-07-06');
 
 INSERT INTO itinerary 
     (day, destination, leisure, dining, transport, budget)
