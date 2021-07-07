a77b1305112ba2495ad5e3013fdc44dfbbc0180b
Nick Aldridge <naldridge1360@gmail.com>
2021-07-07 14:22:34 -0500

    mvc to display trip history

--- models/tripsModel.js
+++ models/tripsModel.js
@@ -4,7 +4,25 @@ const db = require('./conn');
 
 
 class tripsModel {
-    constructor(id, location, )
+    constructor(id, location, trip_name, trip_date, user_id) {
+        this.id = id;
+        this.location = location;
+        this.trip_name = trip_name;
+        this.trip_date = trip_date;
+        this.user_id = user_id;
+    }
+
+    static async getTripbyUser() {
+        try {
+            const response = await db.any(
+                `SELECT * FROM trips WHERE user_id = ${this.user_id};`
+            )
+                return response;
+        } catch (err) {
+            console.error("Error: ", err);
+            return err;
+        }
+    }
 }
 
 
