e896afa67a19b75d0d796edd5c18901615fd0253
David Vasek <david.vasek51@gmail.com>
2021-07-07 15:22:33 -0500

    added users controller

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 const http = require('http');
 
@@ -42,50 +41,3 @@ app.get('/favicon.ico', (req, res) => {
 
 app.use('/', rootController);
 app.use('/', userController);
-=======
-'use strict';
-
-const http = require('http');
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const express = require('express');
-const session = require('express-session');
-const app = express();
-
-const es6Renderer = require('express-es6-template-engine');
-app.engine('html', es6Renderer);
-app.set('views', './views');
-app.set('view engine', 'html');
-
-app.use(
-    session({
-        secret: 'Safe Travels!',
-        resave: false,
-        saveUninitialized: false,
-        is_logged_in: false
-    })
-);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-    console.log(`Server is running on http://${hostname}:${port}`);
-});
-
-const rootController = require('./routes/index');
-const userController = require('./routes/users');
-const tripsController = require('./routes/trips');
-
-app.get('/favicon.ico', (req, res) => {
-    res.sendStatus(404);
-})
-
-app.use('/', rootController);
-app.use('/', userController);
-app.use('/', tripsController);
->>>>>>> main
