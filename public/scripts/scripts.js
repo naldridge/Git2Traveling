dd69330c4a143248898e523421dbfe6b5919a215
Nick Aldridge <naldridge1360@gmail.com>
2021-07-08 15:35:29 -0500

    working on the day function

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,7 +1,7 @@
 'use strict'
 
 
-function GetDays() {
+/* function GetDays() {
     let startdt = new Date(document.getElementById("start_date").value);
     let enddt = new Date(document.getElementById("end_date").value);
 
@@ -9,10 +9,52 @@ function GetDays() {
 }
 
 function cal() {
+    let daysnum = Math.abs(GetDays());
+
     if (document.getElementById("start_date")) {
-        document.getElementById("numdays").value = Math.abs(GetDays());
+        document.getElementById("numdays").value = daysnum;
     }
-    console.log(GetDays());
+    return daysnum;
 }
+
+function daysList() {
+    let daysList = [];
+    for (let i = 0; i <= (cal() + 1); i++) {
+        daysList.push(i);
+    }
+
+    return daysList;  
+}
+
+function createDaysDrop(daysList) {
+    let val = daysList[i];
+    let option = document.createElement("option");
+    option.innerText = val;
+    option.value = val;
+    select.AppendChild(option);
+}
+ */
+
+startItinerary.addEventListener(onclick, function(){
+    const start = document.getElementById("start_date").value;
+    const end = document.getElementById("end_date").value;
+    let options = "";
+    for(let day = start; day <=end; day++){
+      options += "<option value=" + day + ">" + day + "</option>"
+    }
+    console.log(options);
+    document.getElementById("day").innerText = options;
+});
+
+function daysList() {
+    let start = document.getElementById("start_date").value;
+    let end = document.getElementById("end_date").value;
+    let options = "";
+    for(let day = start ; day <=end; day++){
+      options += "<option value=" + day + ">"+ day +"</option>";
+    }
+    document.getElementById("day").innerText = options;
+}
+
 
 
