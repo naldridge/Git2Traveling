2080c62c4df996b647c7cf24f6ec212e5ac278e2
David Vasek <david.vasek51@gmail.com>
2021-07-09 16:00:51 -0500

    not sure either

--- routes/trips.js
+++ routes/trips.js
@@ -25,9 +25,31 @@ router.get('/trips_history', async(req, res) => {
         }
     });
 });
+router.get('/trips_plan', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: "Trip Planner",
+            is_logged_in: req.session.is_logged_in
+        },
+        partials: {
+            body: 'partials/trips_plan'
+        }
+    });
+});
+
+router.post('/add_itinerary', (req, res) => {
+    const { day, destination, leisure, dining, transport, budget, trip_id } = req.body;
+    const itinerary = new itineraryModel(null, day, destination, leisure, dining, transport, budget, trip_id);
+    const response = itinerary.addItinerary();
+    if (response.rowCount >= 1) {
+        res.redirect('back');
+    } else {
+        res.sendStatus(500);
+    }
+});
 
 
-router.post('/add_trip',  (req, res) => {
+router.post('/add_trip', (req, res) => {
     const { location, trip_name, trip_start_date, trip_end_date, user_id } = req.body;
     const trip = new tripsModel(null, location, trip_name, trip_start_date, trip_end_date, user_id);
     const response = trip.addTrip();
