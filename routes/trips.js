bb8d7048f8cd477dbaff1acb76634569be40add0
David Vasek <david.vasek51@gmail.com>
2021-07-12 12:11:29 -0500

    changed post partial view get

--- routes/trips.js
+++ routes/trips.js
@@ -9,14 +9,22 @@ const itineraryModel = require('../models/itineraryModel');
 
 
 router.get('/history', async(req, res) => {
-
     const user_id = req.session.user_id;
-
-    //const trips = new tripsModel();
     const trips_history = await tripsModel.getTripByUser(user_id);
     console.log("trips_history: ", trips_history);
-});
 
+    res.render('template', {
+        locals: {
+            title: "Trip History",
+            trip: trips_history,
+            is_logged_in: req.session.is_logged_in,
+            user_id: req.session.user_id
+        },
+        partials: {
+            body: 'partials/trips_history'
+        }
+    });
+});
 
 router.get('/plan', (req, res) => {
     const user_id = req.session.user_id;
@@ -24,7 +32,8 @@ router.get('/plan', (req, res) => {
     res.render('template', {
         locals: {
             title: "Trip Planner",
-            is_logged_in: req.session.is_logged_in
+            is_logged_in: req.session.is_logged_in,
+            user_id: req.session.user_id,
         },
         partials: {
             body: 'partials/trips_plan'
@@ -43,25 +52,17 @@ router.post('/add_itinerary', (req, res) => {
     }
 });
 
-
-
-
-
-
-
 router.post('/add_trip', async(req, res) => {
     const { location, trip_name, trip_start_date, trip_end_date, user_id } = req.body;
     const trip = new tripsModel(null, location, trip_name, trip_start_date, trip_end_date, user_id);
-    const response = await trip.addTrip();
-    if (response.rowCount >= 1) {
+    console.log("trip: ", trip);
+    const response = await trip.addTrip(location, trip_name, trip_start_date, trip_end_date, user_id);
+    if (!!response.id) {
         res.redirect('back');
     } else {
         res.sendStatus(500);
     }
 
 });
-
-
-
 
 module.exports = router;
